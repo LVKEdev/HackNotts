88cef4bf1f8c05aa6e462dd29a2589c1ff81087a
Luke <54963889+LVKEdev@users.noreply.github.com>
2020-11-29 07:20:34 +0000

    Update Game.java

--- Game.java
+++ Game.java
@@ -4,39 +4,54 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Game {
-    ChoiceHandler cHandler = new ChoiceHandler();
-	Start start = new Start();
-	VisibilityManager vm = new VisibilityManager(start);
-	
-	public static void main(String[] args) {
-		
-		new Game();
-		
-	}
-	
-	public Game() {
-		start.drawGUI(cHandler);
-		vm.showTitleScreen();
-		
-	}
-	
-	public class ChoiceHandler implements ActionListener{
+  ChoiceHandler     cHandler = new ChoiceHandler();
+  Start             start    = new Start();
+  VisibilityManager vm       = new VisibilityManager(start);
 
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			
-			String yourChoice = arg0.getActionCommand();
-			
-			switch(yourChoice) {
-			case "start": vm.titleToMain(); break;
-			case "accept": vm.accept(); break;
-			case "reject": vm.reject(); break;
-			case "cashIn": vm.cashIN(); break;
-			}
-			
-			
-		}
-		
-	}
-	
+  public static void main(String[] args) {
+
+    new Game();
+
+  }
+
+  public Game() {
+    start.drawGUI(cHandler);
+    vm.showTitleScreen();
+
+  }
+
+  public class ChoiceHandler implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+
+      String yourChoice = arg0.getActionCommand();
+
+      switch (yourChoice) {
+        case "start":
+          System.out.println("here");
+          vm.titleToMain();
+         // vm.titleToInstructions();
+          break;
+
+          case "go":
+         // vm.instructionsToMain();
+          break;
+
+        case "accept":
+          vm.accept();
+          break;
+        case "reject":
+          vm.reject();
+          break;
+        case "cashIn":
+          vm.cashIN();
+          break;
+      }
+
+
+    }
+
+  }
+
 }
