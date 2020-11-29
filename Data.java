101cb8afcb9488912daae6dca6e5ecb3a7b81f8a
Luke <54963889+LVKEdev@users.noreply.github.com>
2020-11-29 07:19:49 +0000

    Update Data.java

--- Data.java
+++ Data.java
@@ -1,22 +1,64 @@
 package bank;
 
-import java.util.ArrayList;
-
 public class Data {
-	
-	public static String[] names = {"Steve", "Rachel", "Sharon"};
-	public static String[] funds = {"£150", "£250", "£100"};
-	public static int[] credit = {500, 300, 100};
-	
-	
-	public static String[] pitch = {"Wants to invest £100 in a company providing inflatable love dolls.",
-			"Wants to invest £150 in an obviously crooked pyramid scheme.",
-			"Wants to invest £5 in a a chewing gum that tastes of Dominic Cummings."};
-	public static int[] fundsRequired = {100, 150, 5};
-	public static float[] riskiness = {(float) 0.7, (float) 0.5, (float) 0.2};
-	}
 
-    
+  public static String[] names =
+      {"Sammy The Squirrel", "Berney the Bear", "Spike Lee the Snail",
+       "Terry the Turtle", "Harry the Hamster", "Percy the Frog",
+       "Amy the Anaconda",
+
+       };
 
 
+  public static String[] funds  = {"150", "250", "14000", "100", "5000",
+                                   "23300", "25000"};
+  public static int[]    credit = {500, 350, 450, 450, 550, 700, 750};
 
+
+  public static String[] pitch        =
+      {"Sammy the Squirrel is amazed at how many " +
+       "squirrels are unable to feed their " +
+       "families. Sammy has begun stock piling nuts and is " +
+       "looking to invest in a restaurant to feed his community. " +
+       "The money will cover the building costs, marketing and storage of the" +
+       " nuts.",
+
+       "Berney is an established DJ whose career has taken a hit from the " +
+       "current lockdown. He is looking to have a career change into cyber security. " +
+       "He has been hacking for over 15 years for BI5(Bear Intelligence ). He" +
+       " is asking for 2,000 for a new laptop and new desk for his new-found " +
+       "career.",
+
+       "Spike has recently graduated from Berkeley and moved to the UK. He " +
+       "wants to set up his own movie production company. He has been acting " +
+       "for all his life and now would like to become a director and produce " +
+       "his own movies.",
+
+       "Terry has been working full time for many years. He has a very slow " +
+       "paced job that has been driving him crazy. He wants to move into the " +
+       "fast lane and start a new business with his friend Mr Hare. They would like to start a racing track that allows people to race with their favourite designer cars.",
+
+       //5
+       "Harry has been a cleaner for many years and is looking to franchise " +
+       "his business. He would need the money to help with staffing, relocation and buildings.",
+
+       "Currently starred in the latest Disney romcom the princess and the " +
+       "frog. Since then he has had his big break and has been working on various acting project. " +
+       "He loves acting and is now looking to teach people all the skills that he has learnt. " +
+       "He would like to start a consulting firm in New York. He would need " +
+       "the money for marketing and staffing.",
+
+       "Amy has been a therapist for 15 years. She wants to develop an app to" +
+       " keep track of her clients. She believes that it would be a great way to bring in more clients and patients.",
+
+
+       };
+  public static float[]  rewardAmount = {1.4f, 1.5f, 3f, 3f, 1.8f, 2.2f, 1.5f};
+
+  public static int[]   fundsRequired = {1000, 2000, 5000, 6000, 4000, 3000,
+                                         2500};
+  public static float[] riskiness     =
+      {1.4f, 1.4f, 0.6f, 0.7f, 1.2f, 1.0f, 0.7f
+
+      };
+}
